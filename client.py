cd6bac894c70f08239d3181150be97097e7dd590
Cinderrr <86509478+Cinderrr@users.noreply.github.com>
2021-06-28 22:46:01 -0600

    dictSend Update

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
 import socket
+import json
+import getpass
 
 HEADER_LENGTH = 512
 
@@ -6,15 +8,33 @@ IP = "127.0.0.1"
 PORT = 3000
 
 #Basic login function that takes the given ID at face value. Highly insecure. Will need to update with sessions down the line
-def loginID():
-    ID = input("Sign in with your ID: ")
-    return ID
+def loginBase():
+    signChoice = input("Hello! Would you like to sign-up or sign in:\n1. Sign-Up\n2. Sign-In\n")
+    if signChoice == '1':
+        user = loginSignUp()
+    elif signChoice == '2':
+        user = loginSignIn()
+    else:
+        print("Invalid choice. Exiting...")
+    
+    return user
+
+def loginSignIn():
+    username = input("Username: ")
+    password = getpass.getpass()
+    user = {username, password}
+
+    return user
+
+def loginSignUp():
+    username = input("Username: ")
+    password = getpass.getpass()
+    user =  {username, password}
+
+    return user
 
 #Player starts a game. Will be prompted to select from Rock Paper Scissors or quiting.
 def startGame(client_socket, ID):
-    WTD = '0'
-    gameMode = '0'
-    betAmount = '0'
 
     #Prompting player for this before contacting the server side. Get everything now and send it in one quick burst of messages.
     gameMode = input("What would you like to play?\n0. Exit\n1. Rock Paper Scissors\n")
@@ -33,32 +53,31 @@ def startGame(client_socket, ID):
     betAmount = input("What would you like to bet?\n")
 
     #Invalid amount once again exits software
-    if betAmount > '0':
-        print("You have bet " + betAmount + " SFM")
-    else:
-        print("Invalid amount entered. Exiting...")
+    try:
+        if int(betAmount) > 0 :
+            print("You have bet ", betAmount, " SFM")
+        else:
+            print("Invalid amount entered. Exiting...")
+            exit()
+    except:
+        print("Invalid entry. Exiting...")
         exit()
 
-    #What to do. Will communicate with server what the next incoming data is. State machine or something like that.
-    #Harmless if someone fucks with the code. All it'll do is error out and kill their software once exception handling is added to the server side
-    WTD                 = WTD.encode('utf-8')
-    WTD_header          = f"{len(WTD):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(WTD_header + WTD)
+    sendDict = {
+        "WTD": "0",
+        "ID": ID,
+        "gameMode": gameMode,
+        "betAmount": betAmount
+    }
 
-    #ID. Once again super insecure. But sessions are gonna be a thing. Then it'll be more secure
-    ID                  = ID.encode('utf-8')
-    ID_header           = f"{len(ID):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(ID_header + ID)
-
-    #Gamemode. Harmless to fuck with. It'll just error out the dipshit's client side
-    gameMode            = gameMode.encode('utf-8')
-    gameMode_header     = f"{len(gameMode):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(gameMode_header + gameMode)
-
-    #Amount bet. Take a guess
-    betAmount           = betAmount.encode('utf-8')
-    betAmount_header    = f"{len(betAmount):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(betAmount_header + betAmount)
+    try:
+        sendDict = str(sendDict)
+        sendDict = sendDict.encode('utf-8')
+        sendDict_header = f"{len(sendDict):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(sendDict_header + sendDict)
+    except:
+        print("There has been an issue sending you information. Exiting...")
+        return None
 
     #Currently have only RPS. Will want to make this a state machine once new gamemodes are added
     clientRPS(client_socket)
@@ -72,23 +91,25 @@ def joinGame(client_socket, ID):
     #########################################
     lobbyID = input("What is the lobby's id: ")
 
-    #WTD. Same as startGame()
-    WTD                 = WTD.encode('utf-8')
-    WTD_header          = f"{len(WTD):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(WTD_header + WTD)
+    sendDict = {
+        "WTD": '1',
+        "ID" : ID,
+        "lobbyJID": lobbyID 
+    }
 
-    #ID. Will need to change
-    ID                  = ID.encode('utf-8')
-    ID_header           = f"{len(ID):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(ID_header + ID)
-
-    #LobbyID. Explained above
-    lobbyID             = lobbyID.encode('utf-8')
-    lobbyID_header      = f"{len(lobbyID):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(lobbyID_header + lobbyID)
+    try:
+        sendDict = str(sendDict)
+        sendDict = sendDict.encode('utf-8')
+        sendDict_header = f"{len(sendDict):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(sendDict_header + sendDict)
+    except:
+        print("There has been an issue sending you information. Exiting...")
+        return None
 
     #Player starts the game. Will need to switch to a state machine at some point.
     clientRPS(client_socket)
+
+    return None
 
 #Rock paper scissors. It works as far as I can tell
 def clientRPS(client_socket):
@@ -130,21 +151,42 @@ def clientRPS(client_socket):
                 return False
             results_length = int(results_header.decode('utf-8'))
             results = client_socket.recv(results_length).decode('utf-8')
-            print(results)
+            
+        if results == '-1':
+            print("Tie! Go again...")
+            #Tie
+            #Loop again
+        elif results == '0':
+            print("Player one wins!")
+            loopContinue = 0
+            #P1
+            #Kill loop
+        elif results == '1':
+            print("Player two wins!")
+            loopContinue = 0
+            #P2
+            #Kill loop
+        elif results == '2':
+            print("One or more invalid entries. Go again...")
 
     return None
 
 #This function checks the amount of money the passed ID has. Not too dangerous to have without a session, but would be QoL and protect people from phishing attacks
 def checkAmount(client_socket, ID):
-    WTD = '2'
     
-    WTD                 = WTD.encode('utf-8')
-    WTD_header          = f"{len(WTD):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(WTD_header + WTD)
-
-    sendID              = ID.encode('utf-8')
-    sendID_header       = f"{len(sendID):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(sendID_header + sendID)
+    sendDict = {
+        "WTD": '2',
+        "ID": ID
+    }
+    
+    sendDict = str(sendDict)
+    sendDict = sendDict.encode('utf-8')
+    try:
+        sendDict_header = f"{len(sendDict):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(sendDict_header + sendDict)
+    except:
+        print("There has been an issue retrieving you balance...")
+        return None
 
     message_header = client_socket.recv(HEADER_LENGTH)
     if not len(message_header):
@@ -152,54 +194,61 @@ def checkAmount(client_socket, ID):
     message_length = int(message_header.decode('utf-8'))
     message = client_socket.recv(message_length).decode('utf-8')
     print(message)
+
     return None
 
 #This function checks the available games there are to join. Totally safe to not have a session check as far as I can tell
 def checkGames(client_socket):
     WTD = '3'
     
-    WTD                 = WTD.encode('utf-8')
-    WTD_header          = f"{len(WTD):<{HEADER_LENGTH}}".encode('utf-8')
-    client_socket.send(WTD_header + WTD)
-
-    loop_header = client_socket.recv(HEADER_LENGTH)
-    if not len(loop_header):
-        return False
-    loop_length = int(loop_header.decode('utf-8'))
-    loop = client_socket.recv(loop_length).decode('utf-8')
-
     try:
-        loop = int(loop)
-    except:
-        print("No lobbies available...")
-        return None
-    
-    for x in range(int(loop)):
-        message_header = client_socket.recv(HEADER_LENGTH)
-        if not len(message_header):
+        WTD                 = WTD.encode('utf-8')
+        WTD_header          = f"{len(WTD):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(WTD_header + WTD)
+
+        loop_header = client_socket.recv(HEADER_LENGTH)
+        if not len(loop_header):
             return False
-        message_length = int(message_header.decode('utf-8'))
-        message = client_socket.recv(message_length).decode('utf-8')
-        print(message + "\n")
+        loop_length = int(loop_header.decode('utf-8'))
+        loop = client_socket.recv(loop_length).decode('utf-8')
+
+        try:
+            loop = int(loop)
+        except:
+            print("No lobbies available...")
+            return None
+        
+        for x in range(int(loop)):
+            message_header = client_socket.recv(HEADER_LENGTH)
+            if not len(message_header):
+                return False
+            message_length = int(message_header.decode('utf-8'))
+            message = client_socket.recv(message_length).decode('utf-8')
+            print(message + "\n")
+    except:
+        print("There has been an issue retrieving available games...")
 
     return None
-
+ID = 0
 
 #Start of main
 #############################################################
-ID = loginID()
+user = loginBase()
+
+print(user)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
 
-WTD = input("What would you like to do: ")
-if WTD == '0':
-    startGame(client_socket, ID)
-elif WTD == '1':
-    joinGame(client_socket, ID)
-elif WTD == '2':
-    checkAmount(client_socket, ID)
-elif WTD == '3':
-    checkGames(client_socket)
-else:
-    print("Invalid entry... ")
+while True:
+    WTD = input("What would you like to do: ")
+    if WTD == '0':
+        startGame(client_socket, ID)
+    elif WTD == '1':
+        joinGame(client_socket, ID)
+    elif WTD == '2':
+        checkAmount(client_socket, ID)
+    elif WTD == '3':
+        checkGames(client_socket)
+    else:
+        print("Invalid entry... ")
--- server.py
+++ server.py
@@ -1,65 +1,51 @@
 import socket
 import select
 import json
+import ast
 
 lobbies = []
 
 HEADER_LENGTH = 512
 #Database values are placeholders until an actual server is set-up 
-databsePull = './../Database/Files/users.json'
-databasePush = './../Database/Files/user.json'
+databsePull = 'C:/Users/Cinder/Documents/Mooncade/Database/Files/users.json'
+databasePush = 'C:/Users/Cinder/Documents/Mooncade/Database/Files/user.json'
 
 IP = "127.0.0.1"
 PORT = 3000
 
-
 #Kept this here for reusage. Honestly not really needed anymore. I'll kill this another day. Dont worry about it
 #######################################################
 def receive_message(client_socket):
-
     try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-        if not len(message_header):
+        recvDict_header = client_socket.recv(HEADER_LENGTH)
+        if not len(recvDict_header):
+            print("Fuck")
             return False
-        message_length = int(message_header.decode('utf-8').strip())
-
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-
+        recvLength = int(recvDict_header.decode('utf-8').strip())
+        recvDict = client_socket.recv(recvLength).decode('utf-8')
+        returnDict = ast.literal_eval(recvDict)
     except:
         return False
+    return returnDict
+
+def verifyUser(client_socket):
+    print("Verifying...")
+    #RSA. Oh no.
+
+
+
 
 #This is triggered when an incoming connection wants to create a lobby
 #######################################################
-def lobbyCreate(client_socket):
-    #gather incoming connections ID
-    ID_header = client_socket.recv(HEADER_LENGTH)
-    if not len(ID_header):
-        return False
-    ID_length = int(ID_header.decode('utf-8').strip())
-    ID = client_socket.recv(ID_length).decode('utf-8')
-
-    #gather incoming connections desired gamemode
-    gameMode_header = client_socket.recv(HEADER_LENGTH)
-    if not len(gameMode_header):
-        return False
-    gameMode_length = int(gameMode_header.decode('utf-8').strip())
-    gameMode = client_socket.recv(gameMode_length).decode('utf-8')
-
-    #gather incoming connections desired amount to bet
-    #Will need to create a check for the account. Make sure they arent spending more than they have
-    betAmount_header = client_socket.recv(HEADER_LENGTH)
-    if not len(gameMode_header):
-        return False
-    betAmount_length = int(betAmount_header.decode('utf-8').strip())
-    betAmount = client_socket.recv(betAmount_length).decode('utf-8')
+def lobbyCreate(client_socket, recvDict):
 
     #Stores the new lobby in a dict value
     newLobby = {
         "lobbyID": str(len(lobbies)),
         "params": {
-            "con1": [client_socket, ID],
-            "gameMode": gameMode,
-            "betAmount": betAmount
+            "con1": [client_socket, recvDict['ID']],
+            "gameMode": recvDict['gameMode'],
+            "betAmount": recvDict['betAmount']
         }
     }
 
@@ -70,23 +56,11 @@ def lobbyCreate(client_socket):
 
 #Joins lobby. WIll retrieve ID and lobby to join. Currently bugged so that you can join a lobby that doesnt exist
 #######################################################
-def lobbyJoin(client_socket):
-    
-    ID_header = client_socket.recv(HEADER_LENGTH)
-    if not len(ID_header):
-        return False
-    ID_length = int(ID_header.decode('utf-8').strip())
-    ID = client_socket.recv(ID_length).decode('utf-8')
-    
-    lobbyJID_header = client_socket.recv(HEADER_LENGTH)
-    if not len(lobbyJID_header):
-        return False
-    lobbyJID_length = int(lobbyJID_header.decode('utf-8').strip())
-    lobbyJID = client_socket.recv(lobbyJID_length).decode('utf-8')
+def lobbyJoin(client_socket, recvDict):
 
     for i in lobbies:
-        if i['lobbyID'] == lobbyJID:
-            i['params']['con2'] = [client_socket, ID],
+        if i['lobbyID'] == recvDict['lobbyJID']:
+            i['params']['con2'] = [client_socket, recvDict['ID']],
             lobbyRPS(i)
             lobbies.remove(i)
             return None
@@ -155,27 +129,23 @@ def lobbyRPS(lobby):
         results = calculateResults(playerOneResponse, playerTwoResponse)
 
         if results == '-1':
-            print("Tie")
             message = "Tie! Go again...".encode('utf-8')
             message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
             #Tie
             #Loop again
         elif results == '0':
-            print("P1")
             message = "Player one wins!".encode('utf-8')
             message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
             loopContinue = 0
             #P1
             #Kill loop
         elif results == '1':
-            print("P2")
             message = "Player two wins!".encode('utf-8')
             message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
             loopContinue = 0
             #P2
             #Kill loop
         elif results == '2':
-            print("Invalid")
             message = "One or more invalid entries. Go again...".encode('utf-8')
             message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
 
@@ -188,28 +158,20 @@ def lobbyRPS(lobby):
         playerOne.send(results_header + results)
         playerTwo.send(results_header + results)
 
-        print("\n", lobby['params']['con1'][1])
-        print("\n", lobby['params']['con2'][0][1])
-
         winnerAlter(int(lobby['params']['con1'][1]), int(lobby['params']['con2'][0][1]), int(lobby['params']['betAmount']))
 
     return None
 
 #Take a guess. Based solely on the good graces of someone not signing in on other than their own ID. Will need ***SESSIONS*** 
 #######################################################
-def checkAmount(client_socket):
-    message_header = client_socket.recv(HEADER_LENGTH)
-    if not len(message_header):
-        return False
-    message_length = int(message_header.decode('utf-8'))
-    message = client_socket.recv(message_length).decode('utf-8')
+def checkAmount(client_socket, recvDict):
     
     json_file = open(databasePush, 'r')
     data = json.load(json_file)
     json_file.close()
 
     try:
-        sendAmount = f"You have {str(data['accounts'][int(message)]['amount'])} SFM"
+        sendAmount = f"You have {str(data['accounts'][int(recvDict['ID'])]['amount'])} SFM"
         sendAmount = sendAmount.encode('utf-8')
         sendAmount_header = f"{len(sendAmount):<{HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(sendAmount_header + sendAmount)
@@ -279,23 +241,23 @@ while True:
 
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
+    print("test")
+
     for notified_socket in read_sockets:
         if notified_socket == server_socket:
             client_socket, client_address = server_socket.accept()
             
-            WTD = receive_message(client_socket)
-            if WTD is False:
-                continue
+            recvDict = receive_message(client_socket)
     
             print(f"Accepted new connection from {client_address[0]}:{client_address[1]}")
-            WTD = WTD['data'].decode('utf-8')
-            if WTD == '0':
-                lobbyCreate(client_socket)
-            elif WTD == '1':
-                lobbyJoin(client_socket)
-            elif WTD == '2':
-                checkAmount(client_socket)
-            elif WTD == '3':
+
+            if recvDict['WTD'] == '0':
+                lobbyCreate(client_socket, recvDict)
+            elif recvDict['WTD'] == '1':
+                lobbyJoin(client_socket, recvDict)
+            elif recvDict['WTD'] == '2':
+                checkAmount(client_socket, recvDict)
+            elif recvDict['WTD'] == '3':
                 checkGames(client_socket)
             else:
                 print("Yeah, nah...")
